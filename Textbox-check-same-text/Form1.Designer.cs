f024a76d82e67fbf87d83905e18a66848a70981f
ChanyanuchNai02 <chanyanuch.nai@gmail.com>
2025-06-19 00:35:03 +0700

    Upload Full Code

---
+++ Textbox-check-same-text/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace TextboxCheckSameTextApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblResult;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+
+            // textBoxA
+            this.textBoxA.Location = new System.Drawing.Point(30, 30);
+            this.textBoxA.Name = "A";
+            this.textBoxA.Size = new System.Drawing.Size(240, 22);
+
+            // textBoxB
+            this.textBoxB.Location = new System.Drawing.Point(30, 90);
+            this.textBoxB.Name = "B";
+            this.textBoxB.Size = new System.Drawing.Size(240, 22);
+
+            // btnCheck
+            this.btnCheck.Location = new System.Drawing.Point(100, 60);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(100, 25);
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+
+            // Result
+            this.lblResult.Location = new System.Drawing.Point(300, 30);
+            this.lblResult.Name = "Result";
+            this.lblResult.Size = new System.Drawing.Size(200, 90);
+            this.lblResult.Text = "";
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResult.BackColor = System.Drawing.Color.LightBlue;
+            this.lblResult.ForeColor = System.Drawing.Color.Navy;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(300, 150);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.textBoxB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+
+            // Update Form Size and Add Controls
+            this.ClientSize = new System.Drawing.Size(550, 180);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblResult);
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Textbox-check-same-text/Form1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Windows.Forms;
+
+namespace TextboxCheckSameTextApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            textBoxB.Text = textBoxA.Text;
+        }
+    
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if (textBoxA.Text == textBoxB.Text)
+            {
+                lblResult.Text = "Both text are same";
+            }
+            else
+            {
+                lblResult.Text = "Text not same";
+            }
+        }
+
+    }
+}
